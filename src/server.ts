204ab5e93e8e15be0e62b4a17116285fbf15f670
ahmedWessamTest <ahmedwessam202@gmail.com>
2025-09-24 12:01:34 +0200

    new

--- src/server.ts
+++ src/server.ts
@@ -1,93 +1,67 @@
-// import {
-//   AngularNodeAppEngine,
-//   createNodeRequestHandler,
-//   isMainModule,
-//   writeResponseToNodeResponse,
-// } from '@angular/ssr/node';
-// import express from 'express';
-// import { dirname, resolve } from 'node:path';
-// import { fileURLToPath } from 'node:url';
+import { APP_BASE_HREF } from '@angular/common';
+import { CommonEngine, isMainModule } from '@angular/ssr/node';
+import express from 'express';
+import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import bootstrap from './main.server';
 
-// const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-// const browserDistFolder = resolve(serverDistFolder, '../browser');
+const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+const browserDistFolder = resolve(serverDistFolder, '../browser');
+const indexHtml = join(serverDistFolder, 'index.server.html');
 
-// const app = express();
-// const angularApp = new AngularNodeAppEngine();
+const app = express();
+const commonEngine = new CommonEngine();
 
-// /**
-//  * Example Express Rest API endpoints can be defined here.
-//  * Uncomment and define endpoints as necessary.
-//  *
-//  * Example:
-//  * ```ts
-//  * app.get('/api/**', (req, res) => {
-//  *   // Handle API request
-//  * });
-//  * ```
-//  */
+/**
+ * Example Express Rest API endpoints can be defined here.
+ * Uncomment and define endpoints as necessary.
+ *
+ * Example:
+ * ```ts
+ * app.get('/api/**', (req, res) => {
+ *   // Handle API request
+ * });
+ * ```
+ */
 
-// /**
-//  * Serve static files from /browser
-//  */
-// app.use(
-//   express.static(browserDistFolder, {
-//     maxAge: '1y',
-//     index: false,
-//     redirect: false,
-//   }),
-// );
+/**
+ * Serve static files from /browser
+ */
+app.get(
+  '**',
+  express.static(browserDistFolder, {
+    maxAge: '1y',
+    index: 'index.html'
+  }),
+);
 
-// /**
-//  * Handle all other requests by rendering the Angular application.
-//  */
-// app.use('/**', (req, res, next) => {
-//   angularApp
-//     .handle(req)
-//     .then((response) =>
-//       response ? writeResponseToNodeResponse(response, res) : next(),
-//     )
-//     .catch(next);
-// });
+/**
+ * Handle all other requests by rendering the Angular application.
+ */
+app.get('**', (req, res, next) => {
+  const { protocol, originalUrl, baseUrl, headers } = req;
 
-// /**
-//  * Start the server if this module is the main entry point.
-//  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
-//  */
-// if (isMainModule(import.meta.url)) {
-//   const port = process.env['PORT'] || 4000;
-//   app.listen(port, () => {
-//     console.log(`Node Express server listening on http://localhost:${port}`);
-//   });
-// }
+  commonEngine
+    .render({
+      bootstrap,
+      documentFilePath: indexHtml,
+      url: `${protocol}://${headers.host}${originalUrl}`,
+      publicPath: browserDistFolder,
+      providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+    })
+    .then((html) => res.send(html))
+    .catch((err) => next(err));
+});
 
-// /**
-//  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
-//  */
-// export const reqHandler = createNodeRequestHandler(app);
+/**
+ * Start the server if this module is the main entry point.
+ * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+ */
+if (isMainModule(import.meta.url)) {
+  const port = process.env['PORT'] || 4000;
+  app.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
 
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-
-const angularAppEngine = new AngularAppEngine();
-
-export const handler = async (event: any, context: any) => {
-  try {
-    const request = new Request(event.rawUrl, {
-      method: event.httpMethod,
-      headers: event.headers,
-      body: event.body,
-    });
-
-    const response = await angularAppEngine.handle(request, context);
-
-    return {
-      statusCode: response.status,
-      headers: Object.fromEntries(response.headers),
-      body: await response.text(),
-    };
-  } catch (err: any) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message }),
-    };
-  }
-};
+export default app;
